be6590c01c4d69ab9f4ead9deb8c239da713b205
Adipa Wijayathilaka <adipamihi@gmail.com>
2018-01-31 22:54:11 -0500

    Added WIP pages.  Changed navbar to use Flask's templating engine.

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/reports/')
+def reports():
+	return render_template('reports.html')
+
+@app.route('/agents/')
+def agents():
+	return render_template('agents.html')
+
+@app.route('/manual/')
+def manual():
+	return render_template('manual.html')
